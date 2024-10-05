9f7b468e6644d7160774690f41baed497bcb9bf2
saniya <saniya@DESKTOP-TF8BM6A>
2024-10-05 19:01:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace HandmadeClothesBoutique
+{
+    using System;
+
+    // Custom exception class for product out of stock
+    public class ProductOutOfStockException : Exception
+    {
+        public ProductOutOfStockException(string message) : base(message) { }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class DeliveryService
+    {
+        public bool PlaceOrder(Product product)
+        {
+            if (product.Stock <= 0)
+            {
+                throw new ProductOutOfStockException("Product is out of stock");
+            }
+            else
+            {
+                product.Stock--; // decrementing stop
+                return true;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DeliveryService deliveryService = new DeliveryService();
+
+            Console.Write("Enter the product name: ");
+            string productName = Console.ReadLine();
+            Console.Write("Enter the number of stocks: ");
+            int stock = Convert.ToInt32(Console.ReadLine());
+
+            Product product = new Product { Name = productName, Stock = stock };
+
+            try
+            {
+                if (deliveryService.PlaceOrder(product))
+                {
+                    Console.WriteLine("Order placed successfully");
+                }
+            }
+            catch (ProductOutOfStockException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
